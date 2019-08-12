93ccc67e518f9c78f8015372caa0050c36763152
PalinaP <alexandra.porcher2@gmail.com>
2019-08-12 17:41:59 +0200

    mail sender

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,5 +2,10 @@ class Attendance < ApplicationRecord
   belongs_to :event
   belongs_to :participant, class_name: 'User'
 
+  after_create :attendance_confirmation_email
+
+  def attendance_confirmation_email
+    UserMailer.attendance_confirmation_email(self.participant, self.event).deliver_now
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   has_many :attended_events, :through => :attendances, :source => :event
   has_many :organized_events, foreign_key: 'organizor_id', class_name: 'Event', dependent: :destroy
 
+  after_create :welcome_email
+
+  def welcome_email
+    UserMailer.welcome_email(self).deliver_now
+  end
+
+
 end
